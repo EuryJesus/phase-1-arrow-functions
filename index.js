54d6522651f758570644c73890edd411b87901b3
EuryJesus <Eury0218@hotmail.com>
2023-04-08 02:58:10 -0400

    Adds index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+function divide() {
+    let a = 2000;
+    let b = 100;
+    console.log(2000/100);
+    const devide = ('2000, 100')
+    console.log(2000 / 100);
+    return 20
+}
+
+
+function square() {
+    let multiply = n => n * 2;
+    const nums = [1,2,3];
+const squares = nums.map(x => x ** 2); 
+squares; 
+console.log(2*2)
+return 4
+   }
+function add () {
+    
+    let parameter1 = 'a';
+    let parameter2 = 'b';
+    let b = 3;
+    let a = 4;
+
+    const add = (parameter1, parameter2) => parameter1 + parameter2;
+
+        return 7
+}
